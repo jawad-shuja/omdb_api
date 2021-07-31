f440cb0460bbba96a0eb581ac851e6a2a5f849cd
Jawad Shuja <jawad.shuja@gmail.com>
2021-08-01 02:13:34 +0500

    API: Add standard resource response
    WHAT: Add render_response to application controller.
    
    WHY: To standardise 200 and 422 responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# app/controllers/application_controller.rb
+
 class ApplicationController < ActionController::API
+
+  def render_response(resource, blueprint)
+    if resource.errors.empty?
+      render blueprint.render(resource)
+    else
+      render json: {
+        status: "failed",
+        messages: resource.errors.full_messages
+      }, status: 422
+    end
+  end
+
 end
