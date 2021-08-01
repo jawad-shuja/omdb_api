729ebd7c87608e758e0bc5ee7501ce91b8f02b26
Jawad Shuja <jawad.shuja@gmail.com>
2021-08-01 22:52:31 +0500

    Documentation: Add user apis to swagger
    WHAT: Define docs for sessions and registrations APIs.
    
    WHY: For documentation completion.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,36 @@
 # app/controllers/registrations_controller.rb
 
 class RegistrationsController < Devise::RegistrationsController
+  include Swagger::Blocks
+
+  swagger_path '/api/signup' do
+    operation :post do
+      key :description, 'Sign up'
+      key :produces, [
+        'application/json'
+      ]
+      key :tags, [
+        'user'
+      ]
+
+      parameter do
+        key :name, :user
+        key :in, :body
+        key :description, 'User to register'
+        key :required, true
+        schema do
+          key :'$ref', :UserInput
+        end
+      end
+
+      response 200 do
+        key :description, 'User response'
+        schema do
+          key :'$ref', :User
+        end
+      end
+    end
+  end
 
   def create
     build_resource(sign_up_params)
